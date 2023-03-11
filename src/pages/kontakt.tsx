c280da0d049e333acd984dcae7d44980f26cf292
Matěj Pešl <matejpesl1@gmail.com>
2023-03-11 13:16:01 +0100

    change

--- src/pages/kontakt.tsx
+++ src/pages/kontakt.tsx
@@ -2,16 +2,17 @@ import React from "react"
 import {NextPage} from "next";
 import {useSession} from "next-auth/react";
 import License from "./licence";
+import {ArticleDiv, ParTitle} from "../components/article";
+import PageTitle from "../components/PageTitle";
 
 const Contact: NextPage = () => {
     const session = useSession();
 
     return (
-        <div>
-            <h1>Kontakt</h1>
+        <ArticleDiv>
+            <PageTitle>Kontaktujte nás</PageTitle>
 
-            todo - implement contact page
-        </div>
+        </ArticleDiv>
     )
 }
 export default Contact;
