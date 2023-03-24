2ef994af83b4c021ed6abdebf318c5d9d532d88f
Matěj Pešl <matejpesl1@gmail.com>
2023-03-24 22:15:46 +0100

    alter error messages in middleware to correct them according to older implementation of zod

--- src/pages/api/trpc/[trpc].ts
+++ src/pages/api/trpc/[trpc].ts
@@ -7,10 +7,29 @@ import { createContext } from "../../../server/context";
 export default trpcNext.createNextApiHandler({
     router: appRouter,
     createContext: createContext,
-    onError:
-        process.env .NODE_ENV === "development"
-            ? ({ path, error }) => {
-                console.error(`❌ tRPC failed on ${path ?? "<no-path>"}: ${error.message}`);
-            }
-            : undefined,
+    onError({ error, type, path, input, ctx, req }) {
+        if (process.env .NODE_ENV === "development") {
+            console.error(`❌ tRPC failed on ${path ?? "<no-path>"}: ${error.message}`);
+        }
+
+        if (error.code === 'INTERNAL_SERVER_ERROR') {
+            // TODO: send to bug reporting.
+
+        }
+
+        // Change the error message because in new versions of tRPC there is a
+        // changed behavior where error.message is not the message, but rather an array
+        // with error objects (which contain the error messages) in string format, even if there is
+        // only one.
+        //
+        // That could be good for debugging, but we are already using the message property to show
+        // user friendly errors in the frontend.
+        //
+        // Thats why we will alter the error message here - if it is an array, we will extrac
+        // all the messages and join them with a comma.
+        if (error.message[0] === "[") {
+            const messages = (JSON.parse(error.message) as any[]).map((error) => error.message);
+            error.message = messages.join(" • ");
+        }
+    },
 });
