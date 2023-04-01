01c07143afb07aaf4d6ba9b16a9d472b9cab2464
Matěj Pešl <matejpesl1@gmail.com>
2023-04-01 23:46:58 +0200

    pre-add code for edge-runtime switch

--- src/pages/api/trpc/[trpc].ts
+++ src/pages/api/trpc/[trpc].ts
@@ -1,12 +1,55 @@
 import * as trpcNext from '@trpc/server/adapters/next';
 import { appRouter } from '../../../server/routers/_app';
 import { createContext } from "../../../server/context";
+import {NextRequest, NextResponse} from "next/server";
+import {fetchRequestHandler} from "@trpc/server/adapters/fetch";
 
 // export API handler
 // @see https://trpc.io/docs/api-handler
 
 
-// TODO: Get rid of bcrypt package and switch to edge.
+// TODO: switch to edge.
+
+// We're using the edge-runtime
+/*export const config = {
+    runtime: 'edge',
+};
+
+// export API handler
+export default async function handler(req: NextRequest, res: NextResponse) {
+    return fetchRequestHandler({
+        endpoint: '/api/trpc',
+        router: appRouter,
+        req,
+        createContext: createContext,
+
+        onError({ error, type, path, input, ctx, req }) {
+            if (process.env .NODE_ENV === "development") {
+                console.error(`❌ tRPC failed on ${path ?? "<no-path>"}: ${error.message}`);
+            }
+
+            if (error.code === 'INTERNAL_SERVER_ERROR') {
+                // TODO: send to bug reporting.
+            }
+
+            // Change the error message because in new versions of tRPC there is a
+            // changed behavior where error.message is not the message, but rather an array
+            // with error objects (which contain the error messages) in string format, even if there is
+            // only one.
+            //
+            // That could be good for debugging, but we are already using the message property to show
+            // user friendly errors in the frontend.
+            //
+            // Thats why we will alter the error message here - if it is an array, we will extrac
+            // all the messages and join them with a comma.
+            if (error.message[0] === "[") {
+                const messages = (JSON.parse(error.message) as any[]).map((error) => error.message);
+                error.message = messages.join(" • ");
+            }
+        },
+    });
+}*/
+
 export default trpcNext.createNextApiHandler({
     router: appRouter,
     createContext: createContext,
--- src/server/context.ts
+++ src/server/context.ts
@@ -3,6 +3,7 @@ import * as trpcNext from '@trpc/server/adapters/next';
 import { getSession } from 'next-auth/react';
 import {Configuration, OpenAIApi} from "openai";
 import mongooseConnect from "../lib/mongooseConnect";
+import {FetchCreateContextFnOptions} from "@trpc/server/adapters/fetch";
 
 
 const configuration = new Configuration({apiKey: process.env.OPENAI_API_KEY});
@@ -11,7 +12,7 @@ const openai = new OpenAIApi(configuration);
  * Creates context for an incoming request
  * @link https://trpc.io/docs/context
  */
-export async function createContext(opts?: trpcNext.CreateNextContextOptions) {
+export async function createContext(opts?: trpcNext.CreateNextContextOptions | FetchCreateContextFnOptions ) {
     // Do this terrible hack so that we can create context in SSG Helpers. If a procedure is used for SSG with this context,
     // it MUST NOT use session data or opts, because they might be null.
     opts = opts as trpcNext.CreateNextContextOptions;
