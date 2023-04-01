74c4dc16cb8630669f4b64fb567dfd97223760e8
Matěj Pešl <matejpesl1@gmail.com>
2023-04-01 11:56:04 +0200

    add keys to meta tags

--- src/components/PageHead.tsx
+++ src/components/PageHead.tsx
@@ -4,7 +4,7 @@ import {PropsWithChildren} from "react";
 export default function PageHead(props: PropsWithChildren<{title: string, description?: string}>) {
     return (
         <Head>
-            <title>{props.title + " | Open Tools"}</title>
+            <title key={"page-title"}>{props.title + " | Open Tools"}</title>
             {props.description && <meta key={"page-description"} name="description" content={props.description} />}
             {props.children}
         </Head>
