fda3f0616b5b5a0ccf7b5e3e87e62bbdf14c0f97
Matěj Pešl <matejpesl1@gmail.com>
2023-04-08 12:29:30 +0200

    convert to docker

--- next.config.js
+++ next.config.js
@@ -1,5 +1,7 @@
 /** @type {import('next').NextConfig} */
 module.exports = {
+  // To support docker.
+  output: 'standalone',
   reactStrictMode: true,
   compiler: {
     // Enables the styled-components SWC transform
