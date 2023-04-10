40cffe48c705e4b39f19704ceb5c1df2e9d3b669
Matěj Pešl <matejpesl1@gmail.com>
2023-04-10 10:08:37 +0200

    redirect www -> non-www

--- next.config.js
+++ next.config.js
@@ -1,6 +1,16 @@
 /** @type {import('next').NextConfig} */
 module.exports = {
   reactStrictMode: true,
+  async redirects() {
+    return [
+      {
+        source: '/:path*',
+        has: [{ type: 'header', key: "host", value: 'www.intellify.cz' }],
+        destination: 'https://intellify.cz/:path*',
+        permanent: true
+      }
+    ]
+  },
   compiler: {
     // Enables the styled-components SWC transform
     styledComponents: true
